05c774182bb2ed367822e9c9b2d855fe3fd798c4
Ryan Walmsley <ryanteck@gmail.com>
2019-10-26 12:01:38 +0100

    Fixed GPIO Access

--- controller/files/run.sh
+++ controller/files/run.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
+
+#This script runs on the boot of the container
+
+#We need to check that 1) There is an SSL Certificate and 2) The Cert has 7 Days left on it
+
+if [ ! -f "/opt/iotloragateway/ssl/iotloragateway.key" ]; then
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /opt/iotloragateway/ssl/iotloragateway.key -out /opt/iotloragateway/ssl/iotloragateway.crt  -subj "/C=GB/ST=Kent/L=Tunbridge Wells/O=Global Security/OU=IT Department/CN=iotloragateway"
+fi
+if [ ! -f "/opt/iotloragateway/ssl/dhparam.pem" ]; then
+  openssl dhparam -out /opt/iotloragateway/ssl/dhparam.pem 2048
+fi
+
 service php7.3-fpm start
 nginx -g "daemon off;"
