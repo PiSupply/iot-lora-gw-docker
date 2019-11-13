571aaa52177b5ae0d7c4d54e6c8c86e98c2c86bc
Ryan Walmsley <ryanteck@gmail.com>
2019-11-13 22:40:57 +0000

    Fix

--- packet_forwarder/files/configurePktFwd.py
+++ packet_forwarder/files/configurePktFwd.py
@@ -2,19 +2,19 @@
 #Configures the packet forwarder based on the YAML File and Env Variable
 import os
 import yaml
-moduleId = os.environ['LORAMODULE']
+moduleId = int(os.environ['LORAMODULE'])
 
-print(moduleId)
 
 
 
 with open("/opt/iotloragateway/config/gateway_configuration.yml", 'r') as stream:
     try:
-        if(moduleId = 0):
+        if(moduleId == 0):
             config = yaml.safe_load(stream)['packet-forwarder-1']
-        elif(moduleId = 1):
+        elif(moduleId == 1):
             config = yaml.safe_load(stream)['packet-forwarder-2']
         else:
             die()
     except yaml.YAMLError as exc:
         print(exc)
+print(config)
