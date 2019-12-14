6575fe6d87db0970e20bd6541cc817aa19ea485a
Ryan Walmsley <ryanteck@gmail.com>
2019-12-14 13:16:35 +0000

    Start docker container

--- iot-lora-gw-docker-run.sh
+++ iot-lora-gw-docker-run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#Start docker container
 docker-compose up
--- iot-lora-gw-docker-update.sh
+++ iot-lora-gw-docker-update.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+#Move into directory
 cd /opt/iotloragateway/iot-lora-gw-docker
+#Pull latest updates from git
 git pull
+#Pull latest containers from docker
 docker-compose pull
+#Update service
 install -m 644 iot-lora-gateway.service /lib/systemd/system/
+#Reload services
 systemctl daemon-reload
+#Re-enable service
 systemctl enable iot-lora-gateway.service
+
+#Update 4G Configuration
