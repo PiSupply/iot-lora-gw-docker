6f12959dc67adc9d038ec47cd8ed0076ee476978
Ryan Walmsley <ryanteck@gmail.com>
2020-03-16 10:29:59 +0000

    Change update

--- iot-lora-gw-docker-run.sh
+++ iot-lora-gw-docker-run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+#Move into directory
+cd /opt/iotloragateway/iot-lora-gw-docker
+#Update the OS
+apt-get update
+apt-get upgrade -y
+
+#Pull latest containers from docker
+docker-compose pull
+
 #Start docker container
 docker-compose up
--- iot-lora-gw-docker-update.sh
+++ iot-lora-gw-docker-update.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 #Move into directory
 cd /opt/iotloragateway/iot-lora-gw-docker
-#Update the OS
-apt-get update
-apt-get upgrade -y
 #Pull latest updates from git
 git pull
-#Pull latest containers from docker
-docker-compose pull
-#Update service
-install -m 644 iot-lora-gateway.service /lib/systemd/system/
-#Reload services
-systemctl daemon-reload
-#Re-enable service
-systemctl enable iot-lora-gateway.service
 
 /opt/iotloragateway/iot-lora-gw-docker/4GInserted.sh
 
