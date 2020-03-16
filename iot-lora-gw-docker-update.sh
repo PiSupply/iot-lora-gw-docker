b2ac9535af901b68ef5134296d705356835f0576
Ryan Walmsley <ryanteck@gmail.com>
2020-03-16 10:32:05 +0000

    Add newer image

--- iot-lora-gw-docker-update.sh
+++ iot-lora-gw-docker-update.sh
@@ -4,6 +4,10 @@ cd /opt/iotloragateway/iot-lora-gw-docker
 #Pull latest updates from git
 git pull
 
+#Reset GPIO Pins
+python3 gpioReset.py
+
+#Run 4G Part
 /opt/iotloragateway/iot-lora-gw-docker/4GInserted.sh
 
 #Python Script that updates the 4G Config
